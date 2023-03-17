17e27c164a14b540bf9ec09a5f72deac7d75eaf8
bomanaps <mercy.boma35@yahoo.com>
2023-03-18 00:15:23 +0100

    test update

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { type } from 'snarkyjs';
+import { Mina } from "@o1labs/client-sdk";
 
 type PublicKey = {
   id: string;
@@ -20,19 +20,15 @@ type DIDDocument = {
   service: Service[];
 }
 
-type DIDRegistryState = {
-  documents: Map<string, DIDDocument>;
-}
-
-
 class DIDRegistry {
-  private documents: Map<string, DIDDocument> = new Map<string, DIDDocument>();
 
-  public create_did(did: string, context: string[], public_keys: PublicKey[], services: Service[]): void {
-    if (this.documents.has(did)) {
-      throw new Error("DID already exists");
-    }
+  private client: Mina;
 
+  constructor(endpoint: string, privateKey: string) {
+    this.client = new Mina(endpoint, privateKey);
+  }
+
+  public async create_did(did: string, context: string[], public_keys: PublicKey[], services: Service[]): Promise<void> {
     const document: DIDDocument = {
       "@context": context,
       id: did,
@@ -40,57 +36,78 @@ class DIDRegistry {
       service: services,
     }
 
-    this.documents.set(did, document);
+    await this.client.sendTransaction([
+      Mina.createTransactionInstruction(
+        "did_registry",
+        "create_did",
+        { did: did, document: JSON.stringify(document) }
+      )
+    ]);
   }
 
-  public add_public_key(did: string, public_key: PublicKey): void {
-    if (!this.documents.has(did)) {
-      throw new Error("DID does not exist");
-    }
+  public async add_public_key(did: string, public_key: PublicKey): Promise<void> {
+    const document: DIDDocument = await this.get_did_document(did);
 
-    const existing_keys: PublicKey[] = this.documents.get(did)?.publicKeys ?? [];
+    const existing_keys: PublicKey[] = document.publicKeys ?? [];
     if (existing_keys.some(k => k.id === public_key.id)) {
       throw new Error("Public key already exists");
     }
 
     const updated_keys: PublicKey[] = existing_keys.concat(public_key);
-    const updated_document: DIDDocument = { ...this.documents.get(did)!, publicKeys: updated_keys };
-    this.documents.set(did, updated_document);
+    const updated_document: DIDDocument = { ...document, publicKeys: updated_keys };
+
+    await this.client.sendTransaction([
+      Mina.createTransactionInstruction(
+        "did_registry",
+        "update_did",
+        { did: did, document: JSON.stringify(updated_document) }
+      )
+    ]);
   }
 
-  public add_service(did: string, service: Service): void {
-    if (!this.documents.has(did)) {
-      throw new Error("DID does not exist");
-    }
+  public async add_service(did: string, service: Service): Promise<void> {
+    const document: DIDDocument = await this.get_did_document(did);
 
-    const existing_services: Service[] = this.documents.get(did)?.service ?? [];
+    const existing_services: Service[] = document.service ?? [];
     if (existing_services.some(s => s.id === service.id)) {
       throw new Error("Service already exists");
     }
 
     const updated_services: Service[] = existing_services.concat(service);
-    const updated_document: DIDDocument = { ...this.documents.get(did)!, service: updated_services };
-    this.documents.set(did, updated_document);
+    const updated_document: DIDDocument = { ...document, service: updated_services };
+
+    await this.client.sendTransaction([
+      Mina.createTransactionInstruction(
+        "did_registry",
+        "update_did",
+        { did: did, document: JSON.stringify(updated_document) }
+      )
+    ]);
   }
 
-  public get_did_document(did: string): DIDDocument {
-    if (!this.documents.has(did)) {
-      throw new Error("DID does not exist");
-    }
-    return this.documents.get(did)!;
-  }
+  public async get_did_document(did: string): Promise<DIDDocument> {
+    const response = await this.client.callSmartContract(
+      "did_registry",
+      "get_did_document",
+      { did: did }
+    );
 
-  public register_name(name: string, did: string): void {
-    if (this.documents.has(name)) {
-      throw new Error("Name already registered");
-    }
-    if (!this.documents.has(did)) {
-      throw new Error("DID does not exist");
+    if (response.status !== "applied") {
+      throw new Error(`Error getting DID document: ${JSON.stringify(response)}`);
     }
 
-    const document: DIDDocument = this.documents.get(did)!;
-    this.documents.set(name, document);
+    const document: DIDDocument = JSON.parse(response.result);
+
+    return document;
   }
-}
+
+  public async register_name(name: string, did: string): Promise<void> {
+    const document: DIDDocument = await this.get_did_document(did);
+
+    await this.client.sendTransaction([
+      Mina.createTransactionInstruction(
+        "did_registry",
+        "register_name",
+
 
   // update file on test scripts
